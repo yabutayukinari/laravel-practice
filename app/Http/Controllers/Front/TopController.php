a40d176b743ba9a73d799a1e6ef3a202208189eb
yabuta <yabuta@logical-studio.com>
2019-03-12 09:50:06 +0900

    ・不要ファイル削除。 ・コメント追加 ・ide_helper追加 ・TopController追加

---
+++ app/Http/Controllers/Front/TopController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Copyright yabuta
+ * @since     2019/3/11
+ * @package App\Http\Controllers\Front
+ */
+
+namespace App\Http\Controllers\Front;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+/**
+ * Class TopController
+ */
+class TopController extends Controller
+{
+}
