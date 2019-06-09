cee3c02d9314588ef7a084e51eda1655a6b87180
yabuta <yabutayukinari@gmail.com>
2019-06-10 00:38:43 +0900

    ciテスト

--- resources/views/admin/layouts/layouts.blade.php
+++ resources/views/admin/layouts/layouts.blade.php
@@ -4,14 +4,13 @@
     <title>Hello, world!</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport"/>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
     <!--     Fonts and icons     -->
-    <link rel="stylesheet" type="text/css"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 
     <!-- Material Dashboard CSS -->
     <link rel="stylesheet" href="/admin_asset/css/material-dashboard.css">
@@ -22,7 +21,7 @@
     @include('admin.layouts.sidebar')
     <div class="main-panel">
         <div class="content">
-            @yield('content')
+    @yield('content')
         </div>
     </div>
 </div>
